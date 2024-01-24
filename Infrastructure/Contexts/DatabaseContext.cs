e885a06394abc06cdeebd6a0aded6b8d6ffcdbe6
jesper kallioniemi <hwilajesper@live.com>
2024-01-24 13:43:51 +0100

    Worked on DBContext. Done for now! Assigned the unique keys and configured the composite key for UserAddresses

---
+++ Infrastructure/Contexts/DatabaseContext.cs
@@ -0,0 +1,43 @@
+﻿using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Contexts
+{
+    public partial class DatabaseContext(DbContextOptions<DatabaseContext> options) : DbContext(options)
+    {
+        public virtual DbSet<UsersEntity> Users { get; set; }
+        public virtual DbSet<UserAddressesEntity> UserAddresses { get; set; }
+        public virtual DbSet<UserAuthenticationsEntity> UserAuthentications { get; set; }
+        public virtual DbSet<AddressesEntity> Addresses { get; set; }
+        public virtual DbSet<UserProfilesEntity> UserProfiles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Unique keys
+            
+            modelBuilder.Entity<UserProfilesEntity>().
+                HasIndex(u => u.UserId).
+                IsUnique();
+
+            modelBuilder.Entity<UserAuthenticationsEntity>().
+                HasIndex(u => u.Email).
+                IsUnique();
+
+            //Composite key
+            modelBuilder.Entity<UserAddressesEntity>().
+                HasKey(u => new { u.UserId, u.AddressId });
+
+            modelBuilder.Entity<UserAddressesEntity>().
+                HasOne(u => u.User).
+                WithMany(u => u.UserAddresses).
+                HasForeignKey(u => u.UserId);
+
+            modelBuilder.Entity<UserAddressesEntity>().
+                HasOne(u => u.Address).
+                WithMany(u => u.UserAddresses).
+                HasForeignKey(u => u.AddressId);
+        }
+
+    }
+}
+
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//TO-DO: INJECT THE DATABASE STRING CONNECTION WITH DEPENDENCY INJECTION
