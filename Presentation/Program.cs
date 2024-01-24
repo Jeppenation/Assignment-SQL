407398013173c0d23ef945f3bc892376526396c6
jesper kallioniemi <hwilajesper@live.com>
2024-01-24 13:54:21 +0100

    Added the database-string with dependency Injection

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,5 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder();
+
+builder.ConfigureServices(services =>
+{
+   services.AddDbContext<DatabaseContext>(x =>
+   {
+       x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Hwila\source\repos\Assignment-SQL\Infrastructure\Data\AssignmentSQL-CF.mdf;Integrated Security=True;Connect Timeout=30");
+   });
+});
+
+builder.Build();
+
 
 
 //TO-DO: INJECT THE DATABASE STRING CONNECTION WITH DEPENDENCY INJECTION
