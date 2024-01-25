df8b6f5b7d0bfc94c080c6623c41c71d91dc9a63
jesper kallioniemi <hwilajesper@live.com>
2024-01-26 00:06:22 +0100

    Added GetById override to include user information

--- Infrastructure/Repositories/UserProfileRepository.cs
+++ Infrastructure/Repositories/UserProfileRepository.cs
@@ -1,10 +1,18 @@
 ﻿using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace Infrastructure.Repositories;
 
-public class UserProfileRepository(DatabaseContext context) : BaseRepository<UserProfileRepository>(context)
+public class UserProfileRepository(DatabaseContext context) : BaseRepository<UserProfilesEntity>(context)
 {
     private readonly DatabaseContext _context = context;
-    
+
+    public override UserProfilesEntity GetById(Expression<Func<UserProfilesEntity, bool>> predicate)
+    {
+        return _context.UserProfiles.Include(up => up.User).FirstOrDefault(predicate)!;
+    }
 }
